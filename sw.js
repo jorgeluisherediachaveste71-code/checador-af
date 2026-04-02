ef305fd33e2cc10e7224f109f4f50ab1109e2298
jorgeluisherediachaveste71-code <jorgeluisherediachaveste71@gmail.com>
2026-04-02 13:55:05 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'af-checador-v14';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('supabase.co')) return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
